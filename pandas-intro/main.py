06c5aecec8288950b9d42260a42266e60db8bbb1
Aleix-PC <aleix.fort45@gmail.com>
2023-03-11 22:02:55 +0100

    Update

--- pandas-intro/main.py
+++ pandas-intro/main.py
@@ -16,4 +16,14 @@ import pandas
 
 # METHOD 3 (BEST!)
 data3 = pandas.read_csv('./weather_data.csv')
-print(data3['temp'])
+# print(data3)
+temp_list = data3['temp'].to_list()
+# print(temp_list)
+# print(data3['temp'].mean())
+# print(data3[data3.temp == data3.temp.max()])
+# print(data3[data3.temp == 12])
+# print(data3[data3.temp > 20])
+
+monday = data3[data3.day == 'Monday']
+monday_temp = monday.temp
+print(monday_temp)
